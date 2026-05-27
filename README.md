6e519a9156a47c60641727c6610b109d689d65cb
chiarrrra <chiara.leonardi@studenti.isiaurbino.net>
2026-05-27 23:03:31 +0200

    commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Figura di Lissajous
-##
+# Figure di Lissajous
+## Manuel Bove e Chiara Leonardi
+#### progettato e sviluppato da Chiara Leonardi e Manuel Bove con l'obiettivo di ricerca e divulgazione sul tema delle Figure di Lissajous e della fisica dei segnali elettrici ed acustici.<br> ૮꒰ ˶• ༝ •˶꒱ა ♡
+#### Matematica per il design 
+#### Prof. Andreas Gysin
+#### a.a. 2025/2026
+#### ISIA Urbino
--- assets/sketches/oscilloscope/sketch.js
+++ assets/sketches/oscilloscope/sketch.js
@@ -3,53 +3,73 @@ let fxL, fyL, pL;
 let time = 0;
 
 function setup() {
+    // Genera un canvas che si adatta perfettamente alla finestra del tool
     let canvas = createCanvas(windowWidth - 20, 360);
     canvas.parent('canvas-box');
+    
+    // Mappatura selettori dell'interfaccia
     fxS = select('#fx-s'); fyS = select('#fy-s'); pS = select('#p-s');
     fxL = select('#fx-l'); fyL = select('#fy-l'); pL = select('#p-l');
 }
 
 function draw() {
     background(255);
-    fxL.html(fxS.value()); fyL.html(fyS.value()); pL.html(pS.value());
     
-    // Piano cartesiano XY (stile XY)
-    stroke(1); strokeWeight(0.1);
-    for(let i=0; i<width; i+=40) line(i, 0, i, height);
-    for(let j=0; j<height; j+=40) line(0, j, width, j);
+    // Aggiorna dinamicamente i valori numerici scritti nei label dell'HTML
+    fxL.html(fxS.value()); 
+    fyL.html(fyS.value()); 
+    pL.html(pS.value());
     
-    // Assi principali con "tacche" (tick marks)
-    stroke(0); strokeWeight(0.1);
-    line(width/2, 0, width/2, height); // Asse Y
-    line(0, height/2, width, height/2); // Asse X
-    for(let i=0; i<width; i+=40) line(i, height/2 - 4, i, height/2 + 4);
-    for(let j=0; j<height; j+=40) line(width/2 - 4, j, width/2 + 4, j);
+    // Disegno della griglia millimetrata dell'oscilloscopio (passo 20px)
+    stroke(240);
+    strokeWeight(1);
+    for(let i = 0; i < width; i += 20) line(i, 0, i, height);
+    for(let j = 0; j < height; j += 20) line(0, j, width, j);
     
-    let fx = fxS.value();
-    let fy = fyS.value();
-    let ph = radians(pS.value());
-    let amp = 120; // Ampiezza fissa come richiesto
+    // Disegno degli Assi Cartesiani Centrali del Display X-Y
+    stroke(0);
+    strokeWeight(0.1);
+    line(0, height / 2, width, height / 2); // Asse X guidato da CH1
+    line(width / 2, 0, width / 2, height); // Asse Y guidato da CH2
+    
+    // Recupero dati dai potenziometri (cursori slider)
+    let freqCH1 = fxS.value();
+    let freqCH2 = fyS.value();
+    let deltaPhase = radians(pS.value());
+    
+    // Dimensionamento dell'ampiezza per contenere il tracciato nel monitor
+    let ampX = min(width / 2 - 30, 140);
+    let ampY = min(height / 2 - 30, 140);
     
     push();
-    translate(width/2, height/2);
+    translate(width / 2, height / 2); // Sposta l'origine al centro dello schermo
     
-    // Disegna la curva tracciata
-    noFill(); stroke(150); strokeWeight(2);
+    // COMPORTAMENTO VETTORIALE: Disegno della figura di Lissajous complessiva
+    noFill(); 
+    stroke(0); 
+    strokeWeight(1.5);
     beginShape();
-    for(let i=0; i<=TWO_PI; i+=0.01) {
-        let x = amp * sin(fx * i + ph);
-        let y = amp * sin(fy * i);
+    // Campionamento continuo ad alta precisione per chiudere la curva perfettamente
+    for(let i = 0; i <= TWO_PI; i += 0.01) {
+        let x = ampX * sin(freqCH1 * i + deltaPhase);
+        let y = ampY * sin(freqCH2 * i);
         vertex(x, y);
     }
     endShape();
     
-    // Disegna la pallina animata
-    time += 0.03; // Velocità della pallina
-    let bx = amp * sin(fx * time + ph);
-    let by = amp * sin(fy * time);
+    // SIMULAZIONE DEL RAGGIO: Il punto luminoso mobile tracciato in tempo reale
+    time += 0.03; 
+    let pointX = ampX * sin(freqCH1 * time + deltaPhase);
+    let pointY = ampY * sin(freqCH2 * time);
     
-    fill(255); noStroke();
-    ellipse(bx, by, 10, 10);
+    fill(0); 
+    noStroke();
+    ellipse(pointX, pointY, 10, 10); // Spot luminoso dell'oscilloscopio
     
     pop();
+}
+
+// Ridimensiona il canvas se la finestra o l'iframe cambiano dimensione
+function windowResized() {
+    resizeCanvas(windowWidth - 20, 360);
 }
